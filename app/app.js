048d31bb08c1bba6369ccd1ab1554ea9c469a30e
Sonu kafle <146171882+SonuKafle11@users.noreply.github.com>
2026-03-23 16:37:15 +0545

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello from Docker CI/CD Demo 🚀");
+    res.send("Hello from Docker CI/CD Demo");
 });
 
 app.listen(3000, () => {
